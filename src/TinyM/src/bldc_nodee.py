3f0655d3e1938cfeb91bc3f6bf2c9b3345ed7a9a
“Vansh <vanshyadav@live.com>
2023-08-27 16:17:00 +0530

    working ros code

--- src/TinyM/src/bldc_nodee.py
+++ src/TinyM/src/bldc_nodee.py
@@ -24,62 +24,70 @@ from tinymovr.config import get_bus_config, configure_logging
 AXLE_LENGTH = 0.67 # distance between the left and right wheels
 WHEEL_RADIUS = 0.19 # radius of each wheel
 NO_OF_POLE = 4
+
+
 class TinyM:
     
-    AXLE_LENGTH = 0.67 # distance between the left and right wheels
-    WHEEL_RADIUS = 0.19 # radius of each wheel
-    NO_OF_POLE = 4
     
     def __init__(self):
         
         params = get_bus_config()
         params["interface"]="slcan"
         params["bitrate"] = 1000000
-        params["channel"]="/dev/ttyACM0"
+        params["channel"]="/dev/ttyACM1"
         init_tee(can.Bus(**params))
-        tm = create_device(node_id=1)
+        self.tm = create_device(node_id=1)
         time.sleep(0.1)
 
-        tm.encoder.type = 0
+        self.tm.encoder.type = 1
         time.sleep(0.1)
-        print(tm.encoder.type)
-        time.sleep(0.1)
-
-        tm.encoder.bandwidth = 1500
-        time.sleep(0.1)
-        print(tm.encoder.bandwidth)
+        print(self.tm.encoder.type)
         time.sleep(0.1)
 
+        self.tm.encoder.bandwidth = 1500
+        time.sleep(0.1)
+        print(self.tm.encoder.bandwidth)
+        time.sleep(0.1)
 
-        tm.motor.pole_pairs = 4
+
+        self.tm.motor.pole_pairs = 4
         time.sleep(0.1)
-        print(tm.motor.pole_pairs)
+        print(self.tm.motor.pole_pairs)
         time.sleep(0.1)
 
-        tm.controller.position.p_gain = 0.01 #best working as of now
+        self.tm.controller.position.p_gain = 0.01 #best working as of now
         time.sleep(0.1)
-        print(tm.controller.position.p_gain)
+        print(self.tm.controller.position.p_gain)
         time.sleep(0.1)
-        tm.controller.velocity.p_gain = 0.01
+        self.tm.controller.velocity.p_gain = 0.01
         time.sleep(0.1)
-        print(tm.controller.velocity.p_gain)
+        print(self.tm.controller.velocity.p_gain)
         time.sleep(0.1)
-        print(tm.errors)
+        print(self.tm.errors)
         rospy.init_node('motion_control')
         self.cmd_vel_sub = rospy.Subscriber('/cmd_vel',Twist,self.cmd_vel_clbk)
         self.rate = rospy.Rate(10)  # Update rate in Hz
-    
         
+        self.tm.controller.velocity_mode()
+        time.sleep(2)
+        print(self.tm.controller.mode)
+        time.sleep(2)
+        
+        self.tm.controller.velocity.setpoint = 200
+        time.sleep(7)
+        print(self.tm.controller.velocity.setpoint)
+
+        self.tm.controller.idle()
     
     def cmd_vel_clbk(self,msg):
         
-        left_w_vel = msg.linear.x - (msg.angular.x*msg.AXLE_LEN)
-        right_w_vel = msg.linear.x + (msg.angular.x*msg.AXLE_LEN)
+        left_w_vel = msg.linear.x - (msg.angular.x*AXLE_LENGTH)
+        right_w_vel = msg.linear.x + (msg.angular.x*AXLE_LENGTH)
         
         left_w_rpm = (left_w_vel/2*3.14*WHEEL_RADIUS)*60
         right_w_rpm = -(right_w_vel/2*3.14*WHEEL_RADIUS)*60
         
-        self.tm.controller.velocity.setpoint = (left_w_rpm/60)* 24
+        self.tm.controller.velocity.setpoint = (left_w_rpm/60)* 24*60
         
     def cmd_vel_sub(self):
         self.tm.controller.velocity.setpoint = 0
@@ -98,10 +106,10 @@ if __name__ == '__main__':
     try:
         controller = TinyM()
         controller.main()
+        rospy.init_node('bldc_nodee', anonymous=True)
+
     except rospy.ROSInterruptException:
         pass
             
             
-        
-
         
--- src/newcan.py
+++ src/newcan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.10
 
 import time
+import rospy
 
 from tinymovr.tee import init_tee
 from tinymovr.config import  create_device
@@ -30,34 +31,80 @@ time.sleep(0.1)
 
 
 
-tm.encoder.type = 1
+tm.encoder.type = 0
 time.sleep(0.1)
+print(tm.encoder.type)
+time.sleep(0.1)
+
 tm.encoder.bandwidth = 1500
 time.sleep(0.1)
+print(tm.encoder.bandwidth)
+time.sleep(0.1)
+
+
 tm.motor.pole_pairs = 4
 time.sleep(0.1)
-tm.save_config()
-time.sleep(0.1)
-tm.reset() 
+print(tm.motor.pole_pairs)
 time.sleep(0.1)
 
-tm.controller.calibrate()
+#tm.controller.calibrate()
+#time.sleep(1)
+#print(tm.calibrated)
+#time.sleep(0.1)
 
-time.sleep(1)
-print(tm.calibrated)
+tm.controller.position.p_gain = 0.01 #best working as of now
 time.sleep(0.1)
-
-tm.controller.position.p_gain = 0.007 #best working as of now
+print(tm.controller.position.p_gain)
 time.sleep(0.1)
-
-tm.controller.velocity.p_gain = 0.07
+tm.controller.velocity.p_gain = 0.01
 time.sleep(0.1)
+print(tm.controller.velocity.p_gain)
+time.sleep(0.1)
+print(tm.errors)
 
 
 tm.controller.velocity_mode()
 time.sleep(2)
-tm.controller.vel_setpoint = 200
-time.sleep(5)
+print(tm.controller.mode)
+time.sleep(2)
 
+#tm.controller.velocity.setpoint = 200
+#time.sleep(7)
+#print(tm.controller.velocity.setpoint)
+
+#tm.controller.idle()
+
+AXLE_LENGTH = 0.67 # distance between the left and right wheels
+WHEEL_RADIUS = 0.19 # radius of each wheel
+NO_OF_POLE = 4
+
+def cmd_vel_clbk(self,msg):
+        
+        left_w_vel = msg.linear.x - (msg.angular.x*msg.AXLE_LEN)
+        right_w_vel = msg.linear.x + (msg.angular.x*msg.AXLE_LEN)
+        
+        left_w_rpm = (left_w_vel/2*3.14*WHEEL_RADIUS)*60
+        right_w_rpm = -(right_w_vel/2*3.14*WHEEL_RADIUS)*60
+        
+        self.tm.controller.velocity.setpoint = (left_w_rpm/60)* 24
+        
+def cmd_vel_sub(self):
+        self.tm.controller.velocity.setpoint = 0
+        
+        rospy.Subscriber("cmd_vel", Twist, self.cmd_vel_clbk)
+
+        
+def main(self):
+        rospy.loginfo("Robot Motion Control Node started.")
+        while not rospy.is_shutdown():
+            self.rate.sleep()
+            
+        
+#if __name__ == '__main__':
+ #   try:
+        #controller = TinyM()
+        #controller.main()
+  #  except rospy.ROSInterruptException:
+   #     pass
 
 
